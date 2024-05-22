6e58e717fa593fe9615a5d1c7ae74113230848b8
TuLN[LUV] <lyngoctu@luvina.net>
2024-05-22 15:41:57 +0700

    update url BE EC2 ssl

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Api = axios.create({
     // Base url of server side rest apis
-    baseURL: 'http://3.26.222.225/api/',
+    baseURL: 'https://do1tcjzuakcw1.cloudfront.net/api/',
     headers: {
         'Accept': 'application/json'
     }
