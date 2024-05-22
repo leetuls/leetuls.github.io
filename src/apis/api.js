626f8886976d95cad5958e1ddac33c5d2741d9d6
TuLN[LUV] <lyngoctu@luvina.net>
2024-05-22 13:06:42 +0700

    update url backend

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Api = axios.create({
     // Base url of server side rest apis
-    baseURL: 'http://localhost/api/',
+    baseURL: 'http://3.26.222.225/api/',
     headers: {
         'Accept': 'application/json'
     }
